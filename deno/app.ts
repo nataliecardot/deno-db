a358e875e00ef4b79d7aeeab0d09f6bbc7e88c34
Natalie-Cardot <nataliecardot@gmail.com>
2020-08-17 17:07:53 -0700

    Handle CORS errors

--- deno/app.ts
+++ deno/app.ts
@@ -11,6 +11,20 @@ app.use(async (ctx, next) => {
   await next();
 });
 
+// Ensure every outgoing response has proper headers attached
+app.use(async (ctx, next) => {
+  // Which other domains will be allowed to access our resources. *: every domain; every domain may send a request
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  // Which HTTP can be used in requests sent to this backend
+  ctx.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE",
+  );
+  // Which headers may be set by the frontend when it requests data (in the frontend, for PUT and POST requests, Content-Type headers is set to application/JSON). Important because this header on request being sent to Deno server will tell Deno/the Oak framework that the data attached to this request will be in JSON format, which allows Oak to automatically parse it when we try to get access to request body (ctx.request.body), transforming it from JSON to a JS object
+  ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  await next();
+});
+
 app.use(todosRoutes.routes());
 
 app.use(todosRoutes.allowedMethods());
