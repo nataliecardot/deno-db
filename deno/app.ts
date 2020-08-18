93de0b0c4ee6b888b4eb1173be08fc7c0a3e42ec
Natalie-Cardot <nataliecardot@gmail.com>
2020-08-17 17:13:44 -0700

    Add comment

--- deno/app.ts
+++ deno/app.ts
@@ -5,6 +5,7 @@ import todosRoutes from "./routes/todos.ts";
 
 const app = new Application();
 
+// Oak always automatically sends back a respons, when done executing any middleware, not after all middlewares
 // Some route middlewares return promises that are only done when the async stuff in there finished. next() will not wait for it. So whenever you have any middlewares that do async stuff, you should make all your middlewares async and await next(). This tells Oak that we don't only want to start the next middlewares in line, but also want to wait for them to finish before we send back automatically generated response that is sent by Oak whenever it's done executing a middleware. Otherwise, the response bodies set by async route middlewares will not be taken into account
 app.use(async (ctx, next) => {
   console.log("In the middleware!");
