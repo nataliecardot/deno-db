bf98776529d41cbe86ed908ba1521e6c0003a4c2
Natalie-Cardot <nataliecardot@gmail.com>
2020-08-19 00:36:00 -0700

    Finish Deno CRUD operations

--- deno/routes/todos.ts
+++ deno/routes/todos.ts
@@ -10,8 +10,6 @@ interface Todo {
   text: string;
 }
 
-let todos: Todo[] = [];
-
 // ctx object provides access to req and res
 router.get("/todos", async (ctx) => {
   const todos = await getDb().collection("todos").find(); // { _id: ObjectId(), text: '...' }[]
@@ -42,19 +40,24 @@ router.post("/todos", async (ctx) => {
 });
 
 router.put("/todos/:todoId", async (ctx) => {
-  const tid = ctx.params.todoId;
+  // Exclamation mark lets TS know will always be a string (prevents error later)
+  const tid = ctx.params.todoId!;
   const result = ctx.request.body();
   const data = await result.value;
-  const todoIndex = todos.findIndex((todo) => {
-    return todo.id === tid;
-  });
-  todos[todoIndex] = { id: todos[todoIndex].id, text: data.value.text };
+
+  await getDb().collection("todos").updateOne(
+    { _id: ObjectId(tid) },
+    { $set: { text: data.text } },
+  );
+
   ctx.response.body = { message: "Updated todo" };
 });
 
-router.delete("/todos/:todoId", (ctx) => {
-  const tid = ctx.params.todoId;
-  todos = todos.filter((todo) => todo.id !== tid);
+router.delete("/todos/:todoId", async (ctx) => {
+  const tid = ctx.params.todoId!;
+
+  await getDb().collection("todos").deleteOne({ _id: ObjectId(tid) });
+
   ctx.response.body = { message: "Deleted todo" };
 });
 
